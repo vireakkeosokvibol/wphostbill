405c5ca258c3a07bba2765760cbad9c0ae8e9b40
VIREAK KEOSOKVIBOL <46261326+vireakkeosokvibol@users.noreply.github.com>
2021-07-19 17:06:47 +0700

    add setting link on plugin list

--- db.php
+++ db.php
@@ -9,16 +9,32 @@ function wphostbill_create_table() {
   global $wpdb;
   global $wphostbill_db_version;
 
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
   $charset_collate = $wpdb->get_charset_collate();
 
   $wphostbill_table = $wpdb->prefix . 'wphostbill_server';
 
   $sql = "CREATE TABLE IF NOT EXISTS $wphostbill_table(
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
+    hostname VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    port INT(4),
+    account_number MEDIUMINT(9),
+    status BOOLEAN NOT NULL default true,
     PRIMARY KEY (`id`)
   ) $charset_collate;";
-
-  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
   dbDelta($sql);
+
+  $wphostbill_table = $wpdb->prefix . 'wphostbill_server_group';
+
+  $sql = "CREATE TABLE IF NOT EXISTS $wphostbill_table(
+    id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY (`id`)
+  ) $charset_collate;";
+  dbDelta($sql);
+  
   add_option('wphostbill_db_version', $wphostbill_db_version);
 }
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Plugin Name: Woocommerce Hosting Billing
+ * Plugin Name: WP Hosting Billing
  * Description: For for manage and selling web hosting in wordpress woocommerce.
  **/
 include_once('script.php');
@@ -8,5 +9,31 @@ include_once('admin_menu.php');
 include_once('api.php');
 include_once('db.php');
 
+define('WPHOSTBILL', plugin_basename( __FILE__ ));
+
 // Create table when plugion activation is running.
-register_activation_hook( __FILE__, 'wphostbill_create_table' );
+register_activation_hook(__FILE__, 'wphostbill_create_table');
+
+/**
+ * Add settings link to plugin actions
+ *
+ * @param  array  $plugin_actions
+ * @param  string $plugin_file
+ * @since  1.0
+ * @return array
+ */
+function add_plugin_link( $links ) {
+ 
+  $list_table_plugin_url = admin_url("admin.php?page=settings");
+    // $first_link = '<a href="' . $list_table_plugin_url . '">List Table Plugin</a>';
+
+    $settings_link = admin_url("admin.php?page=settings");
+
+    $settings_anchor = '<a href="' . $settings_link . '">Settings</a>';
+
+    array_push($links, $settings_anchor);
+    // array_push($links, $first_link);
+
+    return $links;
+}
+add_filter( 'plugin_action_links_' . WPHOSTBILL, 'add_plugin_link', );
